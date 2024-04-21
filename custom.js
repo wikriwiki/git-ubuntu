394cf4e95791b79708cbd29eddf68b29771a186a
doing_gyu <srdyhtty@naver.com>
2024-04-21 11:25:34 +0900

    FEATURE CLICK EVENT BY JS AT CAT IMAGES

--- custom.js
+++ custom.js
@@ -1,1 +1,9 @@
-alert("경고! 이상 프로그램이 감지되었습니다. \n Secure System in host do not response.\n Are you Sure to conect this Ip?");
+
+const cat = document.querySelector("section");
+
+cat.addEventListener("click", (e)=> {
+    //명령문
+    e.preventDefault();//링크 접속할 떄 바뀌는거 막는 거
+    console.log("고양이를 클릭했습니다.");
+    alert("야옹~");
+});
