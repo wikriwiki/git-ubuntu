0738b82e11e1f131e5999cbe4b6fdec3fffacfa9
doing_gyu <srdyhtty@naver.com>
2024-04-21 11:52:01 +0900

    FEATURE YA OONG WHEM MOUSE ON CAT PIC

--- custom.js
+++ custom.js
@@ -1,9 +1,5 @@
 
-const cat = document.querySelector("section");
-
-cat.addEventListener("click", (e)=> {
-    //명령문
-    e.preventDefault();//링크 접속할 떄 바뀌는거 막는 거
-    console.log("고양이를 클릭했습니다.");
-    alert("야옹~");
+var cat = document.querySelector("#sad_cat");
+cat.addEventListener("mouseenter", (e)=> {
+    alert("야옹");
 });
